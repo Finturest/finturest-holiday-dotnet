e4dd3452d6682d66f7952f75d46b86b8603b6527
art.grabovski <art.grabovski@gmail.com>
2025-05-18 11:01:23 +0200

    Added HolidayServiceClientIntegrationTests

---
+++ tests/Finturest.Holiday.IntegrationTests/HolidayServiceClientGetUpcomingHolidaysIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Finturest.Holiday.Abstractions.Models.Enums;
+
+namespace Finturest.Holiday.IntegrationTests;
+
+public partial class HolidayServiceClientIntegrationTests
+{
+    [Theory]
+    [InlineData("PL", null, null)]
+    [InlineData("PL", 700, null)]
+    [InlineData("PL", 700, HolidayType.Public)]
+    public async Task GetUpcomingHolidaysAsync_HolidaysExist_ReturnCorrectResult(string countryCode, int? days, HolidayType? type)
+    {
+        // Act
+        var result = await _sut.GetUpcomingHolidaysAsync(countryCode, days, type);
+
+        // Assert
+        result.ShouldNotBeEmpty();
+
+        foreach (var holiday in result)
+        {
+            holiday.Name.ShouldNotBeNullOrEmpty();
+            holiday.LocalName.ShouldNotBeNullOrEmpty();
+
+            holiday.Country.ShouldNotBeNull();
+
+            holiday.Country.Name.ShouldNotBeNullOrEmpty();
+            holiday.Country.Alpha2Code.ShouldNotBeNullOrEmpty();
+            holiday.Country.Alpha3Code.ShouldNotBeNullOrEmpty();
+            holiday.Country.NumericCode.ShouldNotBeNullOrEmpty();
+
+            holiday.Types.ShouldNotBeEmpty();
+        }
+    }
+}
